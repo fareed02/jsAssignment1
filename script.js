5ff0057680fc397ea0c28679b2476343c53a94a7
fareed02 <fareedevs@gmail.com>
2023-08-07 21:46:06 +0500

    js first assignemt-1

---
+++ script.js
@@ -0,0 +1,153 @@
+// Chapter 1
+// Q1. Write a script to display following message on your web
+// page:
+
+alert("Error! Please enter a valid passward")
+
+// Q2 Write a script to display following message on your web
+// page: (Hint : Use line break)
+alert ("Welcome to js land...\n Happy Hacking")
+
+// Q3 Write a script to display following messages in sequence:
+
+alert("Welcome to JS Land");
+
+// Q4 Generate the following message through browser’s
+// developer console 
+alert("Hello... I can run JS through my web's browser console")
+
+alert("CHAPTER 2 START")
+// CHAPTER 2
+
+// Q1 Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+let userName = "Jhone Doe"
+alert (userName)
+
+let age = "15 years old"
+alert(age);
+
+let about = "Certified Mobile Application Development"
+alert(about);
+
+let pizzaTag = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(pizzaTag);
+
+// Q2 Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+let email = "My email is fareedevs@gmail.com";
+alert(email);
+
+// Q3 Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box
+ let book = "Am trying to learn a book A smarter way to learn JavaScript";
+ alert(book);
+
+
+ alert("CHAPTER 3 START")
+//  CHAPTER # 3
+//  Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+let track = " You have visited this site 14 times";
+alert(track);
+
+let onlineShpping = "Joh Doe ordered 5 T-shirt(s) on www.xyzClothing.com store"
+alert(onlineShpping);
+
+alert("CHAPTER 4 START")
+
+// CHAPTER # 4
+// Q1. Declare 3 variables in one statement.
+let a = 10 , b = 15 , c = 20;
+
+// Q2. Declare 5 legal & 5 illegal variable names.
+// 5 legal varaibles
+let firstName = "abc";
+let lastName = "xyz";
+let fatherName = "efg";
+let aboutAge = 15;
+let aboutPassion = "Accountant";
+
+// // 5 illegal varaibles
+// let FIRSTname = "true";
+// let class = "2nd";
+// let alert = "this should be alert";
+// let #aboutAge = 15;
+// let function = "about his function";
+
+// Q3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+
+alert("A heading stating “Rules for naming JS variables");
+alert("Variable names can only contain letter, digits,underscores and dollar sign");
+alert("Variable names are case sensative");
+alert("Variable names should not be JS variables and functions");
+
+alert("CHAPTER 5 START")
+
+
+// CHAPTER # 5
+// Q1Write a program that take two numbers & add them in a
+// new variable. Show the result in your browser.
+let num1 = 5;
+let num2 = 3;
+
+// add two numbers
+let sum = num1 + num2;
+
+// display the sum
+console.log(sum);
+
+// Q2Repeat task1 for subtraction, multiplication, division &
+// modulus.
+let num3 = 5;
+let num4 = 3;
+// substract two numbers
+let substract = num1 - num2;
+
+// display the sum
+console.log(substract);
+
+// Q3Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+// b. Show the value of variable in your browser like “Value
+// after variable declaration is: ??”.
+// c. Initialize the variable with some number.
+// d. Show the value of variable in your browser like “Initial
+// value: 5”.
+// e. Increment the variable.
+// f. Show the value of variable in your browser like “Value
+// after increment is: 6”.
+// g. Add 7 to the variable.
+// h. Show the value of variable in your browser like “Value
+
+// MATH EXPRESSIONS | JAVASCRIPT
+
+// Page 2 of 9
+
+// after addition is: 13”.
+// i. Decrement the variable.
+// j. Show the value of variable in your browser like “Value
+// after decrement is: 12”.
+// k. Show the remainder after dividing the variable’s value
+// by 3.
+// l. Output : “The remainder is : 0”.
+let x = 5;
+console.log(x);
+
+let y = 200;
++ + count;
+console.log(y);
+
+
+
+
+
+
